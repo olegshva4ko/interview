7dc23a74e20b4607315f29902280734063a3fc40
olegshva4ko <46941696+olegshva4ko@users.noreply.github.com>
2021-01-23 17:11:01 +0200

    Add files via upload

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -43,7 +43,7 @@ func StartServer(ctx context.Context, config *configs.Config) error {
 	log.Printf("Starting server...")
 	<-ctx.Done()
 	log.Printf("Server stopped")
-	
+
 	server.Cache.WriteFile()
 
 	ctxShutDown, cancel := context.WithTimeout(context.Background(), 5*time.Second)
@@ -101,79 +101,4 @@ func (s *Server) allowRequest(requestsLeft chan struct{}) func() <-chan struct{}
 	}
 }
 
-/*
-resp, err := http.Get("https://api.etherscan.io/api?module=proxy&action=eth_getBlockByNumber&tag=0xafa025&boolean=true&apikey=YourApiKeyToken")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	// body, err := ioutil.ReadAll(resp.Body)
-	// if err != nil {
-	// 	panic(err.Error())
-	// }
 
-	type Value struct {
-		Value string `json:"value"`
-	}
-	type Transactions struct {
-		Values []Value `json:"transactions"`
-	}
-	type Result struct {
-		Result *Transactions `json:"result"`
-	}
-	//	res := &Result{}
-	res1 := &Result{}
-	json.NewDecoder(resp.Body).Decode(&res1)
-	resp.Body.Close()
-	//fmt.Printf("RES1\n%v\n", res1.Result.Values)
-	//json.Unmarshal(body, &res)
-	//fmt.Println(res)
-	var (
-		sum int64
-	)
-	for _, v := range res1.Result.Values {
-		//fmt.Println(v.Value)
-		num, _ := strconv.ParseInt(v.Value[2:], 16, 64)
-		sum += num
-
-	}
-	var (
-		dur1 time.Duration
-		dur2 time.Duration
-		r    float64
-		buff bytes.Buffer
-	)
-	sum = 12
-	for i := 0; i < 1000; i++ {
-		start := time.Now()
-		r = float64(sum) / 1000000000000000000
-		//fmt.Println(result)
-		//fmt.Println("First: ", time.Since(start))
-		dur1 += time.Since(start)
-	}
-
-	fmt.Println("First: ", dur1/1000)
-	for i := 0; i < 1000; i++ {
-		start := time.Now()
-
-		s := fmt.Sprintf("%d", sum)
-		if len(s) >= 18 {
-			buff.WriteString(s[:len(s)-18])
-			buff.WriteString(".")
-			buff.WriteString(s[len(s)-18:])
-		} else {
-			buff.WriteString("0.")
-			for i := 0; i < 18-len(s); i++ {
-				buff.WriteString("0")
-			}
-			buff.WriteString(s)
-		}
-		//fmt.Println(buff.String())
-		dur2 += time.Since(start)
-	}
-
-	fmt.Println("Second: ", dur2/1000)
-
-	os.Exit(0)
-	fmt.Println(r)
-*/
